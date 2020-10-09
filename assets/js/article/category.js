a6b52153d5239e8e2d5e2dd82261ffd50334d711
xjbddd125 <415286088@qq.com>
2020-10-09 20:28:02 +0800

    添加类别

--- assets/js/article/category.js
+++ assets/js/article/category.js
@@ -24,3 +24,32 @@ $('body').on('click', 'button:contains("删除")', function () {
         layer.close(index);
     });
 })
+
+
+//弹层效果
+let addIndex;
+$('button:contains("添加类别")').on('click', function () {
+    addIndex=layer.open({
+        type: 1,
+        title: '调试',
+        content: $('#tpl-add').html(),
+        area: ['500px', '250px']
+    })
+});
+
+//确认添加
+$('body').on('submit', '.add-form', function (e) {
+    e.preventDefault();
+    $.ajax({
+        type: 'POST',
+        url: '/my/article/addcates',
+        data: $(this).serialize,
+        success: function (res) {
+            layer.msg(res.message);
+            if(res.status===0){
+                renderCategory();
+                layer.close(addIndex);
+            }
+        }
+    })
+});
