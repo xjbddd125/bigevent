38bcf10550e9f666bb1758a210e870657dde1163
xjbddd125 <415286088@qq.com>
2020-10-09 18:00:39 +0800

    删除类别

--- assets/js/article/category.js
+++ assets/js/article/category.js
@@ -8,3 +8,19 @@ function renderCategory() {
     })
 }
 renderCategory();
+
+$('body').on('click', 'button:contains("删除")', function () {
+    let id = $(this).data('id');
+    layer.confirm('是否确认删除?', { icon: 3, title: '提示' }, function (index) {
+        $.ajax({
+            url: '/my/article/deletecate/' + id,
+            success: function (res) {
+                if (res.status === 0) {
+                    renderCategory();
+                }
+            }
+        })
+
+        layer.close(index);
+    });
+})
